f847d4de7d59870aba1c3735974717f41787ee5d
Greg Arroyo <greg.arroyo@socreate.it>
2021-08-16 09:06:52 -0700

    flush out schedule days component and calendar date toggling

--- projects/health-app/src/health/schedule/components/schedule-days/schedule-days.component.ts
+++ projects/health-app/src/health/schedule/components/schedule-days/schedule-days.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'schedule-days',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrls: ['schedule-days.component.scss'],
   template: `
     <div class="days">
-      
+      <button
+        type="button"
+        class="day"
+        *ngFor="let day of days; index as i;"
+        (click)="selectDay(i)">
+        <span [class.active]="i === selected">
+          {{ day }}
+        </span>
+      </button>
     </div>
   `
 })
 export class ScheduleDaysComponent {
-  constructor() {}
+  days = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
+
+  @Input()
+  selected: number;
+
+  @Output()
+  select = new EventEmitter<number>();
+
+  selectDay(index: number) {
+    this.select.emit(index);
+  }
 }
